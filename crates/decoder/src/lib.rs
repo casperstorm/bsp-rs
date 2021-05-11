57c5cd11ba95c945abb383a3b31fa3385149d73e
Cory Forsstrom <cforsstrom18@gmail.com>
2021-05-11 16:45:12 -0700

    starting point for decoder

--- crates/decoder/src/lib.rs
+++ crates/decoder/src/lib.rs
@@ -1,1 +1,16 @@
+use std::io::{Read, Seek};
 
+use byteorder::ByteOrder;
+
+mod error;
+pub use error::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct Bsp {}
+
+impl Bsp {
+    pub fn from_reader<R: Read + Seek>(reader: R) -> Result<Self> {
+        Ok(Bsp {})
+    }
+}
