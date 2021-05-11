7afd9c824301dee7d6ff485bd7ae2ad6af47c13a
Cory Forsstrom <cforsstrom18@gmail.com>
2021-05-11 16:35:13 -0700

    basic cli to debug decoder

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,20 @@
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::from_args();
+
+    dbg!(opts);
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt()]
+struct Opts {
+    #[structopt(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Debug, StructOpt)]
+enum Subcommand {
+    /// Decode the supplied .bsp file
+    Decode,
 }
