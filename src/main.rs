2060b62ad72ebfc2d4ed92c36ee46abe7a5212a3
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-05-15 10:33:56 +0200

    load_map moved into ui

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,16 @@ use bevy::prelude::*;
 mod plugins;
 use plugins::ui::UiPlugin;
 
-struct ButtonMaterials {
-    normal: Handle<ColorMaterial>,
-    hovered: Handle<ColorMaterial>,
-    pressed: Handle<ColorMaterial>,
-}
-
-impl FromWorld for ButtonMaterials {
-    fn from_world(world: &mut World) -> Self {
-        let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
-        ButtonMaterials {
-            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
-            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
-            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
-        }
-    }
-}
-
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .init_resource::<ButtonMaterials>()
         .add_plugin(UiPlugin)
         .add_startup_system(setup.system())
-        .add_system(button_system.system())
         .run();
 }
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    button_materials: Res<ButtonMaterials>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
@@ -62,64 +41,4 @@ fn setup(
     });
     // UI camera
     commands.spawn_bundle(UiCameraBundle::default());
-    commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                align_self: AlignSelf::FlexEnd,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    top: Val::Px(15.0),
-                    right: Val::Px(15.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            material: button_materials.normal.clone(),
-            ..Default::default()
-        })
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle {
-                text: Text::with_section(
-                    "Button",
-                    TextStyle {
-                        font: asset_server.load("fonts/iosevka-regular.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                    Default::default(),
-                ),
-                ..Default::default()
-            });
-        });
-}
-
-#[allow(clippy::type_complexity)]
-fn button_system(
-    button_materials: Res<ButtonMaterials>,
-    mut interaction_query: Query<
-        (&Interaction, &mut Handle<ColorMaterial>, &Children),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-) {
-    for (interaction, mut material, children) in interaction_query.iter_mut() {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
-                *material = button_materials.pressed.clone();
-            }
-            Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *material = button_materials.hovered.clone();
-            }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *material = button_materials.normal.clone();
-            }
-        }
-    }
 }
--- src/plugins/ui/mod.rs
+++ src/plugins/ui/mod.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
 
 pub mod fps;
+pub mod load_map;
+
 use fps::FpsPlugin;
+use load_map::LoadMapPlugin;
+
 pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_plugin(FpsPlugin);
+        app.add_plugin(FpsPlugin).add_plugin(LoadMapPlugin);
     }
 }
