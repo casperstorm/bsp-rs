731527ecaf294b3c906957c82bc154cc0835b618
Cory Forsstrom <cforsstrom18@gmail.com>
2021-05-15 14:19:53 -0700

    begin building geometry

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -16,7 +16,40 @@ fn main() {
 
             match decoder.decode_any() {
                 Ok(BspFormat::GoldSrc30(bsp)) => {
-                    dbg!(bsp);
+                    let level_model = bsp.models[0];
+                    let mut level_nodes = vec![bsp.nodes[level_model.idx_head_nodes[0] as usize]];
+                    let mut level_leaves = vec![];
+
+                    while !level_nodes.is_empty() {
+                        let node = level_nodes.pop().unwrap();
+                        let front = node.idx_children[0];
+                        let back = node.idx_children[1];
+
+                        let mut parse = |x: i16| {
+                            if x < -1 {
+                                level_leaves.push(x.abs() - 1);
+                            } else if x >= 0 {
+                                if let Some(n) = bsp.nodes.get(x as usize).copied() {
+                                    level_nodes.push(n);
+                                }
+                            }
+                        };
+
+                        parse(front);
+                        parse(back);
+                    }
+
+                    for leaf_idx in level_leaves {
+                        let leaf = bsp.leaves[leaf_idx as usize];
+
+                        for idx in 0..leaf.num_mark_surfaces as usize {
+                            if let Some(face) =
+                                bsp.faces.get(leaf.idx_first_mark_surface as usize + idx)
+                            {
+                                dbg!(face);
+                            }
+                        }
+                    }
                 }
                 Err(e) => {
                     dbg!(&e);
